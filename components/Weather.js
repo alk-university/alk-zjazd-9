24f8ee279a4749d8aadb9a4205b6e64dce560a01
medynski <wojciech@medynski.io>
2020-06-06 17:03:23 +0200

    Working weather app

--- components/Weather.js
+++ components/Weather.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { css } from '@emotion/native';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
 const Weather = () => {
+  const [loc, setLoc] = useState('Berlin');
+  const [data, setData] = useState(null);
+
+  const handleSearch = () => {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        loc +
+        '&appid=2a1a2f39b94b6bb68a2abb96353ac8b2'
+    )
+      .then((r) => r.json())
+      .then(setData);
+  };
+
+  const getCelsius = (kelvin) => Math.ceil(kelvin - 273.15);
+
   return (
-    <View>
-      <Text>home</Text>
+    <View
+      style={css`
+        padding: 30px;
+      `}
+    >
+      <Text>Lokalizacja:</Text>
+      <TextInput
+        style={css`
+          border: 1px solid #ccc;
+          margin: 20px 0;
+          padding: 10px;
+        `}
+        value={loc}
+        onChangeText={setLoc}
+      />
+
+      <Button onPress={handleSearch} title="Sprawdź pogodę" />
+
+      {data && <Text>Temperatura: {getCelsius(data.main?.temp)} °C</Text>}
     </View>
   );
 };
